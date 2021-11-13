1e14a367521ef215a8a43c1accf961d981ee25e0
Snehil Vijay <snehilvijay@Snehils-MacBook-Pro.local>
2021-11-13 22:28:38 +0530

    Add readme

--- usage.py
+++ usage.py
@@ -1,20 +1,24 @@
 import dash_mantine_components as dmc
-from dash import Dash, Input, Output, html, State, dcc
+from dash import Dash, Input, Output, html
 from datetime import datetime, timedelta
 
 app = Dash(__name__)
 
-
 app.layout = html.Div(
     [
-        dmc.Tooltip(
-            label=" Use this button to save this information in your profile, after that you will be able to access it any time and share it via email.",
-            children=[dmc.Button("Button with a tooltip. Hover on it.")],
-            wrapLines=True,
-            width=220,
-        )
+        dmc.DatePicker(
+            id="datepicker", format="dddd, MMMM D, YYYY", style={"width": "250px"}
+        ),
+        dmc.Space(h=20),
+        dmc.Text(id="text"),
+        dmc.Button("Click Me!"),
     ]
 )
+
+
+@app.callback(Output("text", "children"), Input("datepicker", "date"))
+def datepicker(date):
+    return date
 
 
 if __name__ == "__main__":
