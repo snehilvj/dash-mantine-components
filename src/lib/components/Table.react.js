70b814fc8cc1575bde90c7d3d9e3469975d9183b
emher <emil.h.eriksen@gmail.com>
2021-12-25 17:05:40 +0100

    Proof of concept implementation

--- src/lib/components/Table.react.js
+++ src/lib/components/Table.react.js
@@ -2,28 +2,26 @@ import React from "react";
 import { Table as MantineTable } from "@mantine/core";
 import PropTypes from "prop-types";
 import { pick } from "ramda";
+import {resolveProp} from 'dash-extensions-js'
 
 /**
  * A simple table component. For more information, see: https://mantine.dev/core/table/
  */
 const Table = (props) => {
-    const { rows, columns } = props;
-
-    const ths = (
-        <tr>
-            {columns.map((col, index) => {
-                return <th key={index}>{col}</th>;
-            })}
-        </tr>
-    );
-
-    const trs = rows.map((row, idx1) => (
-        <tr key={idx1}>
-            {row.map((ele, idx2) => {
-                return <td key={idx2 + idx1}>{ele}</td>;
-            })}
-        </tr>
-    ));
+    const { rows, columns, caption, footers, children} = props;
+    // Default render functions.
+    let headerRenderer = (col) => <th>{col}</th>;
+    let cellRenderer = (cell) => <td>{cell}</td>;
+    let footerRenderer = (col) => <td>{col}</td>;
+    // Detect custom render function.
+    const context = null;
+    headerRenderer = props.headerRenderer? resolveProp(props.headerRenderer, context) : headerRenderer;
+    cellRenderer = props.cellRenderer? resolveProp(props.cellRenderer, context) : cellRenderer;
+    footerRenderer = props.footerRenderer? resolveProp(props.footerRenderer, context) : footerRenderer;
+    // Render table.
+    const ths = <tr> {columns.map(headerRenderer)} </tr>;
+    const trs = rows.map((row) => <tr> {row.map(cellRenderer)} </tr>);
+    const tfs = <tr> {footers.map(footerRenderer)} </tr>;
 
     return (
         <MantineTable
@@ -32,9 +30,11 @@ const Table = (props) => {
                 props
             )}
         >
-            <caption>{props.caption}</caption>
+            <caption>{caption}</caption>
             <thead>{ths}</thead>
             <tbody>{trs}</tbody>
+            <tfoot>{tfs}</tfoot>
+            {children}
         </MantineTable>
     );
 };
@@ -45,23 +45,12 @@ Table.defaultProps = {
     captionSide: "bottom",
     rows: [],
     columns: [],
+    footers: [],
 };
 
 Table.propTypes = {
-    /**
-     * Table description
-     */
-    caption: PropTypes.string,
 
-    /**
-     * Table caption position
-     */
-    captionSide: PropTypes.oneOf(["bottom", "top"]),
-
-    /**
-     * Often used with CSS to style elements with common properties
-     */
-    className: PropTypes.string,
+    // Custom Dash props for rendering children
 
     /**
      * Table columns
@@ -69,9 +58,41 @@ Table.propTypes = {
     columns: PropTypes.arrayOf(PropTypes.string),
 
     /**
-     * The ID of this component, used to identify dash components in callbacks
+     * Table rows
      */
-    id: PropTypes.string,
+    rows: PropTypes.arrayOf(PropTypes.array),
+
+    /**
+     * Table footer
+     */
+    footers: PropTypes.arrayOf(PropTypes.string),
+
+    /**
+     * Function that determines how the header cells are rendered
+     */
+    headerRenderer: PropTypes.oneOfType([PropTypes.string, PropTypes.object]),
+
+    /**
+     * Function that determines how the row cells are rendered
+     */
+    cellRenderer: PropTypes.oneOfType([PropTypes.string, PropTypes.object]),
+
+    /**
+     * Function that determines how the footer cells are rendered
+     */
+    footerRenderer: PropTypes.oneOfType([PropTypes.string, PropTypes.object]),
+
+    /**
+     * Table caption
+     */
+    caption: PropTypes.string,
+
+    // Props passed to Mantine
+
+    /**
+     * Table caption position
+     */
+    captionSide: PropTypes.oneOf(["bottom", "top"]),
 
     /**
      * If true row will have hover color
@@ -79,14 +100,26 @@ Table.propTypes = {
     highlightOnHover: PropTypes.bool,
 
     /**
-     * Table rows
-     */
-    rows: PropTypes.arrayOf(PropTypes.array),
-
-    /**
      * If true every odd row of table will have gray background color
      */
     striped: PropTypes.bool,
+
+    /**
+     * Children, can be used to populate the table (instead of columns, rows, footer) for increased flexibility
+     */
+    children: PropTypes.node,
+
+    // Dash props
+
+    /**
+     * Often used with CSS to style elements with common properties
+     */
+    className: PropTypes.string,
+
+    /**
+     * The ID of this component, used to identify dash components in callbacks
+     */
+    id: PropTypes.string,
 
     /**
      * Inline style override
