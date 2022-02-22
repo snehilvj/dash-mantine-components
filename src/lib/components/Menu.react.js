e748c2a880b3379d5af9f055b1a3af5232a130ca
Leonardo Ferreira <ferreira_master@hotmail.com>
2022-02-22 15:31:50 -0300

    updating menu properties

--- src/lib/components/Menu.react.js
+++ src/lib/components/Menu.react.js
@@ -53,78 +53,117 @@ Menu.displayName = "Menu";
 Menu.defaultProps = {};
 
 Menu.propTypes = {
-
     /**
     * dmc.MenuItem() and dmc.Divider() components only, children are passed to MenuBody component
     */
     children: PropTypes.node,
-
     /**
     * 	Arrow distance to the left/right * arrowSize
     */
-    arrowDistance: PropTypes.string,
-
+    arrowDistance: PropTypes.number,
     /**
     * Arrow size in px
     */
-    arrowSize: PropTypes.any,
-
+    arrowSize: PropTypes.number,
     /**
-     * item label
+     * Should menu close on item click
     */
-    closeOnItemClick: PropTypes.node,
-
-
+    closeOnItemClick: PropTypes.bool,
+    /**
+     * Close menu on scroll
+    */
     closeOnScroll: PropTypes.bool,
+    /**
+     * Control prop to get element ref
+    */
     controlRefProp: PropTypes.string,
+    /**
+     * Close delay for hover trigger
+    */
     delay: PropTypes.number,
-
+    /**
+     * Unmount transition duration in ms
+    */
     exitTransitionDuration: PropTypes.number,
-
+    /**
+     * Spacing between element and popper in px
+    */
     gutter: PropTypes.number,
-
+    /**
+     * 	Menu button aria-label and title props
+    */
     menuButtonLabel: PropTypes.string,
-
+    /**
+     * Id attribute of menu
+    */
     menuId: PropTypes.string,
-
+    /**
+     * Called every time menu is closed
+    */
     onClose: PropTypes.func,
-
+    /**
+     * 	Called every time menu is opened
+    */
     onOpen: PropTypes.func,
-
+    /**
+     * Use opened and onClose props to setup controlled menu
+    */
     opened: PropTypes.bool,
-
+    /**
+     * Placement relative to reference element
+    */
     placement: PropTypes.oneOf(["center", "end", "start"]),
-
+    /**
+     * Position relative to reference element
+    */
     position: PropTypes.oneOf(["bottom", "left", "right", "top"]),
-
+    /**
+     * Menu body and items border-radius
+    */
     radius: PropTypes.oneOfType([
         PropTypes.oneOf(["xs", "sm", "md", "lg", "xl"]),
         PropTypes.number,
     ]),
+    /**
+     * Predefined shadow from theme or box-shadow value
+    */
     shadow: PropTypes.oneOf(["xs", "sm", "md", "lg", "xl"]),
-
+    /**
+     * Predefined menu width or number for width in px
+    */
     size: PropTypes.oneOfType([
         PropTypes.oneOf(["auto", "xs", "sm", "md", "lg", "xl"]),
         PropTypes.number,
     ]),
-
-    // transition: PropTypes.bool,
-
+    /**
+     * Mount transition duration in ms
+    */
     transitionDuration: PropTypes.number,
-
+    /**
+     * Mount/unmount transition timing function,
+     *  defaults to theme.transitionTimingFunction
+    */
     transitionTimingFunction: PropTypes.string,
-
+    /**
+     * 	Should focus be trapped when menu is opened
+    */
     trapFocus: PropTypes.bool,
-
+    /**
+     * Event which should open menu
+    */
     trigger: PropTypes.oneOf(["hover", "click"]),
-
+    /**
+     * Renders arrow if true
+    */
     withArrow: PropTypes.bool,
-
+    /**
+     * Whether to render the dropdown in a Portal
+    */
     withinPortal: PropTypes.bool,
-
+    /**
+     * 	Menu body z-index
+    */
     zIndex: PropTypes.number,
-
-
 }
 
 export default Menu;
