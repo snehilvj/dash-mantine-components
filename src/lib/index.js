876d56daf0fbc9e07c5b22e28d0b94ff88791517
Snehil Vijay <snehilvj@outlook.com>
2022-03-05 17:13:26 +0530

    Do not release Menu yet

--- src/lib/index.js
+++ src/lib/index.js
@@ -61,9 +61,6 @@ import Kbd from "./components/Kbd.react";
 import Highlight from "./components/Highlight.react";
 import Timeline from "./components/Timeline.react";
 import TimelineItem from "./components/TimelineItem.react";
-import Menu from "./components/Menu.react";
-import MenuItem from "./components/MenuItem.react";
-import MenuLabel from "./components/MenuLabel.react";
 
 export {
     Button,
@@ -129,7 +126,4 @@ export {
     Highlight,
     Timeline,
     TimelineItem,
-    Menu,
-    MenuItem,
-    MenuLabel,
 };
