720984d254bea5633835b3406a06d54481aa18de
Snehil Vijay <snehilvj@outlook.com>
2022-04-21 22:11:52 +0530

    Add persistence to Chips

--- src/lib/components/Chips.react.js
+++ src/lib/components/Chips.react.js
@@ -36,7 +36,10 @@ const Chips = (props) => {
 
 Chips.displayName = "Chips";
 
-Chips.defaultProps = {};
+Chips.defaultProps = {
+    persisted_props: ["value"],
+    persistence_type: "local",
+};
 
 Chips.propTypes = {
     /**
@@ -113,6 +116,34 @@ Chips.propTypes = {
      * Defined flex-wrap property
      */
     noWrap: PropTypes.bool,
+
+    /**
+     * Used to allow user interactions in this component to be persisted when
+     * the component - or the page - is refreshed. If `persisted` is truthy and
+     * hasn't changed from its previous value, a `value` that the user has
+     * changed while using the app will keep that change, as long as
+     * the new `value` also matches what was given originally.
+     * Used in conjunction with `persistence_type`.
+     */
+    persistence: PropTypes.oneOfType([
+        PropTypes.bool,
+        PropTypes.string,
+        PropTypes.number,
+    ]),
+
+    /**
+     * Properties whose user interactions will persist after refreshing the
+     * component or the page.
+     */
+    persisted_props: PropTypes.arrayOf(PropTypes.oneOf(["value"])),
+
+    /**
+     * Where persisted user changes will be stored:
+     * memory: only kept in memory, reset on page refresh.
+     * local: window.localStorage, data is kept after the browser quit.
+     * session: window.sessionStorage, data is cleared once the browser quit.
+     */
+    persistence_type: PropTypes.oneOf(["local", "session", "memory"]),
 
     /**
      * Defines justify-content property
