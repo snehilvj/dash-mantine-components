a603fcd451826b1e9477b82607b5cb3dd9a985d0
Snehil Vijay <snehilvj@outlook.com>
2022-04-27 09:22:03 +0530

    Add persistence to Switch

--- src/lib/components/Switch.react.js
+++ src/lib/components/Switch.react.js
@@ -28,6 +28,8 @@ Switch.displayName = "Switch";
 
 Switch.defaultProps = {
     checked: false,
+    persisted_props: ["checked"],
+    persistence_type: "local",
 };
 
 Switch.propTypes = {
@@ -80,6 +82,34 @@ Switch.propTypes = {
      * The inner label to be set when Switch is in the checked state
      */
     onLabel: PropTypes.string,
+
+    /**
+     * Used to allow user interactions in this component to be persisted when
+     * the component - or the page - is refreshed. If `persisted` is truthy and
+     * hasn't changed from its previous value, a `value` that the user has
+     * changed while using the app will keep that change, as long as
+     * the new `value` also matches what was given originally.
+     * Used in conjunction with `persistence_type`.
+     */
+    persistence: PropTypes.oneOfType([
+        PropTypes.bool,
+        PropTypes.string,
+        PropTypes.number,
+    ]),
+
+    /**
+     * Properties whose user interactions will persist after refreshing the
+     * component or the page.
+     */
+    persisted_props: PropTypes.arrayOf(PropTypes.oneOf(["checked"])),
+
+    /**
+     * Where persisted user changes will be stored:
+     * memory: only kept in memory, reset on page refresh.
+     * local: window.localStorage, data is kept after the browser quit.
+     * session: window.sessionStorage, data is cleared once the browser quit.
+     */
+    persistence_type: PropTypes.oneOf(["local", "session", "memory"]),
 
     /**
      * Predefined border-radius value from theme.radius or number for border-radius in px
