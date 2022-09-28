ceb9f9f99e3ebf6f8018ad0b84fc29e877b66df4
Snehil Vijay <snehilvj@outlook.com>
2022-09-29 00:34:24 +0530

    Correct blockquote

--- src/ts/components/core/Blockquote.tsx
+++ src/ts/components/core/Blockquote.tsx
@@ -1,17 +1,18 @@
 import React from "react";
-import { DashComponentProps, MantineColors } from "../../props";
+import { DefaultProps } from "../../props";
 import { Blockquote as MantineBlockquote } from "@mantine/core";
+import { MantineColor } from "@mantine/styles";
 
 type Props = {
     /** Icon color from theme */
-    color?: MantineColors;
+    color?: MantineColor;
     /** Icon, defaults to quote icon */
     icon?: React.ReactNode;
     /** Describe a reference to a cited quote */
     cite?: React.ReactNode;
     /** Content */
     children?: React.ReactNode;
-} & DashComponentProps;
+} & DefaultProps;
 
 /**
  * Blockquote with optional cite. For more information, see: https://mantine.dev/core/blockquote/
