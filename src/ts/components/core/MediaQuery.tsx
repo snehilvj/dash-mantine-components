e766902f18286374fb5ef3dd638af963ed8ee33c
Snehil Vijay <snehilvj@outlook.com>
2022-09-29 00:58:02 +0530

    Correct MediaQuery

--- src/ts/components/core/MediaQuery.tsx
+++ src/ts/components/core/MediaQuery.tsx
@@ -1,31 +1,29 @@
 import React from "react";
-import { DashComponentProps } from "../../props";
 import { Box, MediaQuery as MantineMediaQuery } from "@mantine/core";
-import { MantineSize, CSSObject } from "@mantine/styles";
+import { MantineNumberSize, CSSObject } from "@mantine/styles";
 
 type Props = {
-    className?: string;
     /** Child that should be shown at given breakpoint, it must accept className prop */
     children?: React.ReactNode;
     /** Styles applied to child when viewport is smaller than given breakpoint */
-    smallerThan?: MantineSize;
+    smallerThan?: MantineNumberSize;
     /** Styles applied to child when viewport is larger than given breakpoint */
-    largerThan?: MantineSize;
+    largerThan?: MantineNumberSize;
     /** Any other media query */
     query?: string;
     /** Styles applied to child when breakpoint matches */
     styles: Record<string, CSSObject>;
-} & DashComponentProps;
+};
 
 /**
  * Apply styles to children if media query matches. For more information, see: https://mantine.dev/core/media-query/
  */
 const MediaQuery = (props: Props) => {
-    const { children, styles, setProps, ...other } = props;
+    const { children, styles, ...other } = props;
 
     return (
         <MantineMediaQuery styles={styles} {...other}>
-            <Box>{children}</Box>
+            <Box style={{ width: "fit-content" }}>{children}</Box>
         </MantineMediaQuery>
     );
 };
