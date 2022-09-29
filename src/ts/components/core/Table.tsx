9b1fc96032aed44b8a61fdd0c81b87c9c5b47e8f
Snehil Vijay <snehilvj@outlook.com>
2022-09-29 22:35:50 +0530

    Correct table and themeicon

--- src/ts/components/core/Table.tsx
+++ src/ts/components/core/Table.tsx
@@ -1,7 +1,7 @@
 import React from "react";
-import { DashComponentProps, MantineColors } from "../../props";
+import { DefaultProps } from "../../props";
 import { Table as MantineTable } from "@mantine/core";
-import { MantineSize } from "@mantine/styles";
+import { MantineNumberSize } from "@mantine/styles";
 
 type Props = {
     /** If true every odd row of table will have gray background color */
@@ -11,14 +11,18 @@ type Props = {
     /** Table caption position */
     captionSide?: "top" | "bottom";
     /** Horizontal cells spacing from theme.spacing or number to set value in px */
-    horizontalSpacing?: MantineSize;
+    horizontalSpacing?: MantineNumberSize;
     /** Vertical cells spacing from theme.spacing or number to set value in px */
-    verticalSpacing?: MantineSize;
+    verticalSpacing?: MantineNumberSize;
     /** Sets font size of all text inside table */
-    fontSize?: MantineSize;
+    fontSize?: MantineNumberSize;
     /** Children, specifically an HTML representation of the table */
     children?: React.ReactNode;
-} & DashComponentProps;
+    /** Add border to table */
+    withBorder?: boolean;
+    /** Add border to columns */
+    withColumnBorders?: boolean;
+} & DefaultProps;
 
 /**
  * A simple table component. For more information, see: https://mantine.dev/core/table/
--- src/ts/components/core/ThemeIcon.tsx
+++ src/ts/components/core/ThemeIcon.tsx
@@ -1,22 +1,26 @@
 import React from "react";
-import { DashComponentProps, MantineColors } from "../../props";
+import { DefaultProps } from "../../props";
 import { ThemeIcon as MantineThemeIcon, ThemeIconVariant } from "@mantine/core";
-import { MantineSize, MantineGradient } from "@mantine/styles";
+import {
+    MantineNumberSize,
+    MantineColor,
+    MantineGradient,
+} from "@mantine/styles";
 
 type Props = {
     /** Icon */
     children?: React.ReactNode;
     /** Predefined width and height or number for width and height in px */
-    size?: MantineSize;
+    size?: MantineNumberSize;
     /** Predefined border-radius from theme.radius or number for border-radius in px */
-    radius?: MantineSize;
+    radius?: MantineNumberSize;
     /** Icon color from theme */
-    color?: MantineColors;
+    color?: MantineColor;
     /** Controls appearance */
     variant?: ThemeIconVariant;
     /** Controls gradient settings in gradient variant only */
     gradient?: MantineGradient;
-} & DashComponentProps;
+} & DefaultProps;
 
 /**
  * Render icon inside element with theme colors. For more information, see: https://mantine.dev/core/theme-icon/
