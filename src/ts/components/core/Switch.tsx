bf3b7debf6f8609c97cad79c88d0ecc6013cb475
Snehil Vijay <snehilvj@outlook.com>
2023-07-21 22:22:52 +0530

    :sparkles: Add Textarea component

--- src/ts/components/core/Switch.tsx
+++ src/ts/components/core/Switch.tsx
@@ -1,71 +1,70 @@
-
 import React from "react";
 import { MantineProvider, Switch as MantineSwitch } from "@mantine/core";
 import { MantineSize, MantineNumberSize, MantineColor } from "props/mantine";
-import { DashBaseProps, PersistenceProps } from "props/dash"
-import { MantineStylesAPIProps, MantineStyleSystemProps } from "props/mantine"
+import { DashBaseProps, PersistenceProps } from "props/dash";
+import { MantineStylesAPIProps, MantineStyleSystemProps } from "props/mantine";
 
 type Props = {
-  /** Switch label */
-  label?: React.ReactNode;
-  /** Inner label when Switch is in unchecked state */
-  offLabel?: React.ReactNode;
-  /** Inner label when Switch is in checked state */
-  onLabel?: React.ReactNode;
-  /** Switch checked state color from theme.colors, defaults to theme.primaryColor */
-  color?: MantineColor;
-  /** Predefined size value */
-  size?: MantineSize;
-  /** Key of theme.radius or any valid CSS value to set border-radius, "xl" by default */
-  radius?: MantineNumberSize;
-  /** Props spread to wrapper element */
-  wrapperProps?: Record<string, any>;
-  /** Icon inside the thumb of switch */
-  thumbIcon?: React.ReactNode;
-  /** Position of label */
-  labelPosition?: 'left' | 'right';
-  /** description, displayed after label */
-  description?: React.ReactNode;
-  /** Displays error message after input */
-  error?: React.ReactNode;
-  /** A checkbox can show it is currently unable to be interacted with */
-  disabled?: boolean;
-  /** State of check box */
-  checked?: boolean;
+    /** Switch label */
+    label?: React.ReactNode;
+    /** Inner label when Switch is in unchecked state */
+    offLabel?: React.ReactNode;
+    /** Inner label when Switch is in checked state */
+    onLabel?: React.ReactNode;
+    /** Switch checked state color from theme.colors, defaults to theme.primaryColor */
+    color?: MantineColor;
+    /** Predefined size value */
+    size?: MantineSize;
+    /** Key of theme.radius or any valid CSS value to set border-radius, "xl" by default */
+    radius?: MantineNumberSize;
+    /** Props spread to wrapper element */
+    wrapperProps?: Record<string, any>;
+    /** Icon inside the thumb of switch */
+    thumbIcon?: React.ReactNode;
+    /** Position of label */
+    labelPosition?: "left" | "right";
+    /** description, displayed after label */
+    description?: React.ReactNode;
+    /** Displays error message after input */
+    error?: React.ReactNode;
+    /** A checkbox can show it is currently unable to be interacted with */
+    disabled?: boolean;
+    /** State of check box */
+    checked?: boolean;
 } & PersistenceProps &
-  MantineStylesAPIProps &
-  MantineStyleSystemProps &
-  DashBaseProps;
+    MantineStylesAPIProps &
+    MantineStyleSystemProps &
+    DashBaseProps;
 
 /**
  * Capture user feedback limited to small set of options. For more information, see: https://mantine.dev/core/switch/
  */
 const Switch = (props: Props) => {
-  const {
-    setProps,
-    persistence,
-    persisted_props,
-    persistence_type,
-    ...other
-  } = props;
+    const {
+        setProps,
+        persistence,
+        persisted_props,
+        persistence_type,
+        ...other
+    } = props;
 
-  const updateProps = (checked: boolean) => {
-    setProps({ checked });
-  };
+    const updateProps = (checked: boolean) => {
+        setProps({ checked });
+    };
 
-  return (
-    <MantineProvider withNormalizeCSS withGlobalStyles>
-      <MantineSwitch
-        onChange={(ev) => updateProps(ev.currentTarget.checked)}
-        {...other}
-      />
-    </MantineProvider>
-  );
+    return (
+        <MantineProvider withNormalizeCSS withGlobalStyles>
+            <MantineSwitch
+                onChange={(ev) => updateProps(ev.currentTarget.checked)}
+                {...other}
+            />
+        </MantineProvider>
+    );
 };
 
 Switch.defaultProps = {
-  persisted_props: ["checked"],
-  persistence_type: "local",
+    persisted_props: ["checked"],
+    persistence_type: "local",
 };
 
 export default Switch;
