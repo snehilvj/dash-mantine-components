2220fbd34d89136e966b1b2681894996525363f0
Snehil Vijay <snehilvj@outlook.com>
2023-08-02 19:23:59 +0530

    :sparkles: Add Tooltip component

--- src/ts/props/mantine.ts
+++ src/ts/props/mantine.ts
@@ -548,3 +548,28 @@ export type TextProps = {
     /** Shorthand for component="span" */
     span?: boolean;
 };
+
+export type TooltipBaseProps = {
+    /** Target element */
+    children: React.ReactNode;
+    /** Tooltip position relative to target element (default) or mouse (floating) */
+    position?: FloatingPosition;
+    /** Key of the prop that should be used to get element ref */
+    refProp?: string;
+    /** Tooltip label */
+    label: React.ReactNode;
+    /** Determines whether tooltip should be rendered within Portal */
+    withinPortal?: boolean;
+    /** Key of theme.radius or any valid CSS value to set border-radius, theme.defaultRadius by default */
+    radius?: MantineNumberSize;
+    /** Key of theme.colors */
+    color?: MantineColor;
+    /** Defines whether content should be wrapped on to the next line */
+    multiline?: boolean;
+    /** Tooltip width */
+    width?: number | "auto";
+    /** Tooltip z-index */
+    zIndex?: number;
+    /** Disables tooltip */
+    disabled?: boolean;
+};
