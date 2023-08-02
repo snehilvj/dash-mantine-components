d67ef2a1aad3850d7c2011bfa0cda58ae84a76b9
Snehil Vijay <snehilvj@outlook.com>
2023-08-02 16:04:38 +0530

    :sparkles: Add Affix component

---
+++ src/ts/components/core/Affix.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { Affix as MantineAffix } from "@mantine/core";
+import { DashBaseProps } from "props/dash";
+import { MantineStylesAPIProps, MantineStyleSystemProps } from "props/mantine";
+
+type Props = {
+  /** Determines whether component should be rendered within portal, defaults to true */
+  withinPortal?: boolean;
+  /** Affix position on screen, defaults to { bottom: 0, right: 0 } */
+  position?: {
+    top?: string | number;
+    left?: string | number;
+    bottom?: string | number;
+    right?: string | number;
+  };
+  /** Content */
+  children?: React.ReactNode;
+  /** Root element z-index property */
+  zIndex?: number;
+} & DashBaseProps & MantineStylesAPIProps & MantineStyleSystemProps;
+
+/** Render react node inside portal at fixed position */
+const Affix = (props: Props) => {
+  const { children, setProps, ...other } = props;
+
+  return <MantineAffix {...other}>{children}</MantineAffix>;
+};
+
+Affix.defaultProps = {};
+
+export default Affix;
