ed655916bc072c2158b1a4a5f4859b4e481ef27f
Snehil Vijay <snehilvj@outlook.com>
2024-03-09 17:29:33 +0530

    Add Notification componenents

---
+++ src/ts/components/extensions/notifications/NotificationProvider.tsx
@@ -0,0 +1,42 @@
+import { Notifications } from "@mantine/notifications";
+import "@mantine/notifications/styles.css";
+import { BoxProps } from "props/box";
+import { DashBaseProps } from "props/dash";
+import { StylesApiProps } from "props/styles";
+import React from "react";
+
+interface Props extends BoxProps, StylesApiProps, DashBaseProps {
+    /** Notifications position, `'bottom-right'` by default */
+    position?:
+        | "top-left"
+        | "top-right"
+        | "top-center"
+        | "bottom-left"
+        | "bottom-right"
+        | "bottom-center";
+    /** Auto close timeout for all notifications in ms, `false` to disable auto close, can be overwritten for individual notifications in `notifications.show` function, `4000` by defualt */
+    autoClose?: number | false;
+    /** Notification transition duration in ms, `250` by default */
+    transitionDuration?: number;
+    /** Notification width, cannot exceed 100%, `440` by default */
+    containerWidth?: number | string;
+    /** Notification `max-height`, used for transitions, `200` by default */
+    notificationMaxHeight?: number | string;
+    /** Maximum number of notifications displayed at a time, other new notifications will be added to queue, `5` by default */
+    limit?: number;
+    /** Notifications container z-index, `400` by default */
+    zIndex?: string | number;
+    /** Determines whether notifications container should be rendered inside `Portal`, `true` by default */
+    withinPortal?: boolean;
+}
+
+/** Mantine notifications system */
+const NotificationProvider = (props: Props) => {
+    const { setProps, ...other } = props;
+
+    return <Notifications {...other} />;
+};
+
+NotificationProvider.defaultProps = {};
+
+export default NotificationProvider;
