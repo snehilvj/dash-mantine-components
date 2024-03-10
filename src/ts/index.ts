d5c011bf756f5df6e60fc15080bebc9f1faeab44
Snehil Vijay <snehilvj@outlook.com>
2024-03-10 21:43:51 +0530

    Add PieChart

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -30,6 +30,7 @@ import List from "./components/core/list/List";
 import ListItem from "./components/core/list/ListItem";
 import Carousel from "./components/extensions/carousel/Carousel";
 import CarouselSlide from "./components/extensions/carousel/CarouselSlide";
+import PieChart from "./components/extensions/charts/PieChart";
 import RadarChart from "./components/extensions/charts/RadarChart";
 import Sparkline from "./components/extensions/charts/Sparkline";
 import CodeHighlight from "./components/extensions/codehighlight/CodeHighlight";
@@ -73,6 +74,7 @@ export {
     NotificationProvider,
     NumberFormatter,
     Paper,
+    PieChart,
     RadarChart,
     ScrollArea,
     Sparkline,
