3e2a2869b8b1c09f1c3e249c437a60afda92d58d
Snehil Vijay <snehilvj@outlook.com>
2024-03-10 16:05:43 +0530

    Add Timeline but not enabled

---
+++ src/ts/components/core/timeline/Timeline.tsx
@@ -0,0 +1,41 @@
+import {
+    MantineColor,
+    MantineRadius,
+    Timeline as MantineTimeline,
+} from "@mantine/core";
+import { BoxProps } from "props/box";
+import { DashBaseProps } from "props/dash";
+import { StylesApiProps } from "props/styles";
+import React from "react";
+
+interface Props extends BoxProps, StylesApiProps, DashBaseProps {
+    /** `Timeline.Item` components */
+    children?: React.ReactNode;
+    /** Index of active element */
+    active?: number;
+    /** Key of `theme.colors` or any valid CSS color to control active item colors, `theme.primaryColor` by default */
+    color?: MantineColor;
+    /** Key of `theme.radius` or any valid CSS value to set `border-radius`, numbers are converted to rem, `'xl'` by default */
+    radius?: MantineRadius;
+    /** Controls size of the bullet, `20` by default */
+    bulletSize?: number | string;
+    /** Controls how the content is positioned relative to the bullet, `'left'` by default */
+    align?: "right" | "left";
+    /** Control width of the line */
+    lineWidth?: number | string;
+    /** Determines whether the active items direction should be reversed without reversing items order, `false` by default */
+    reverseActive?: boolean;
+    /** Determines whether icon color should depend on `background-color`. If luminosity of the `color` prop is less than `theme.luminosityThreshold`, then `theme.white` will be used for text color, otherwise `theme.black`. Overrides `theme.autoContrast`. */
+    autoContrast?: boolean;
+}
+
+/** Timeline */
+const Timeline = (props: Props) => {
+    const { setProps, children, ...others } = props;
+
+    return <MantineTimeline {...others}>{children}</MantineTimeline>;
+};
+
+Timeline.defaultProps = {};
+
+export default Timeline;
---
+++ src/ts/components/core/timeline/TimelineItem.tsx
@@ -0,0 +1,29 @@
+import { MantineRadius, Timeline } from "@mantine/core";
+import { BoxProps } from "props/box";
+import { DashBaseProps } from "props/dash";
+import { StylesApiProps } from "props/styles";
+import React from "react";
+
+interface Props extends BoxProps, StylesApiProps, DashBaseProps {
+    /** Item title, displayed next to the bullet */
+    title?: React.ReactNode;
+    /** Content displayed below the title */
+    children?: React.ReactNode;
+    /** React node that should be rendered inside the bullet – icon, image, avatar, etc. By default, large white dot is displayed. */
+    bullet?: React.ReactNode;
+    /** Key of `theme.radius` or any valid CSS value to set `border-radius`, numbers are converted to rem, `'xl'` by default */
+    radius?: MantineRadius;
+    /** Controls line border style, `'solid'` by default */
+    lineVariant?: "solid" | "dashed" | "dotted";
+}
+
+/** TimelineItem */
+const TimelineItem = (props: Props) => {
+    const { children, setProps, ...others } = props;
+
+    return <Timeline.Item {...others}>{children}</Timeline.Item>;
+};
+
+TimelineItem.defaultProps = {};
+
+export default TimelineItem;
