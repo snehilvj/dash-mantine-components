fb57329a9563bb2ea5602334140646648a429371
Snehil Vijay <snehilvj@outlook.com>
2024-03-13 18:18:45 +0530

    Add SimpleGrid

---
+++ src/ts/components/core/SimpleGrid.tsx
@@ -0,0 +1,31 @@
+import {
+    SimpleGrid as MantineSimpleGrid,
+    MantineSpacing,
+    StyleProp,
+} from "@mantine/core";
+import { BoxProps } from "props/box";
+import { DashBaseProps } from "props/dash";
+import { StylesApiProps } from "props/styles";
+import React from "react";
+
+interface Props extends BoxProps, StylesApiProps, DashBaseProps {
+    /* Content */
+    children?: React.ReactNode;
+    /** Number of columns, `1` by default */
+    cols?: StyleProp<number>;
+    /** Spacing between columns, `'md'` by default */
+    spacing?: StyleProp<MantineSpacing>;
+    /** Spacing between rows, `'md'` by default */
+    verticalSpacing?: StyleProp<MantineSpacing>;
+}
+
+/** SimpleGrid */
+const SimpleGrid = (props: Props) => {
+    const { children, setProps, ...other } = props;
+
+    return <MantineSimpleGrid {...other}>{children}</MantineSimpleGrid>;
+};
+
+SimpleGrid.defaultProps = {};
+
+export default SimpleGrid;
