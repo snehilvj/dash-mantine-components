ac93d033dd187ef4d27b1b5c56ecb190f7947cbf
Snehil Vijay <snehilvj@outlook.com>
2024-03-13 17:44:52 +0530

    Add Grid components

---
+++ src/ts/components/core/grid/Grid.tsx
@@ -0,0 +1,33 @@
+import { Grid as MantineGrid, MantineSpacing, StyleProp } from "@mantine/core";
+import { BoxProps } from "props/box";
+import { DashBaseProps } from "props/dash";
+import { StylesApiProps } from "props/styles";
+import React from "react";
+
+interface Props extends BoxProps, StylesApiProps, DashBaseProps {
+    /* Content */
+    children?: React.ReactNode;
+    /** Gutter between columns, key of `theme.spacing` or any valid CSS value, `'md'` by default */
+    gutter?: StyleProp<MantineSpacing>;
+    /** Determines whether columns in the last row should expand to fill all available space, `false` by default */
+    grow?: boolean;
+    /** Sets `justify-content`, `flex-start` by default */
+    justify?: React.CSSProperties["justifyContent"];
+    /** Sets `align-items`, `stretch` by default */
+    align?: React.CSSProperties["alignItems"];
+    /** Number of columns in each row, `12` by default */
+    columns?: number;
+    /** Sets `overflow` CSS property on the root element, `'visible'` by default */
+    overflow?: React.CSSProperties["overflow"];
+}
+
+/** Grid */
+const Grid = (props: Props) => {
+    const { children, setProps, ...other } = props;
+
+    return <MantineGrid {...other}>{children}</MantineGrid>;
+};
+
+Grid.defaultProps = {};
+
+export default Grid;
