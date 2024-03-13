10477459667b42804eb0b077232503f018d06221
Snehil Vijay <snehilvj@outlook.com>
2024-03-13 17:08:35 +0530

    Add Stack

---
+++ src/ts/components/core/Stack.tsx
@@ -0,0 +1,27 @@
+import { MantineSpacing, Stack as MantineStack } from "@mantine/core";
+import { BoxProps } from "props/box";
+import { DashBaseProps } from "props/dash";
+import { StylesApiProps } from "props/styles";
+import React from "react";
+
+interface Props extends BoxProps, StylesApiProps, DashBaseProps {
+    /* Content */
+    children?: React.ReactNode;
+    /** Key of `theme.spacing` or any valid CSS value to set `gap` property, numbers are converted to rem, `'md'` by default */
+    gap?: MantineSpacing;
+    /** Controls `align-items` CSS property, `'stretch'` by default */
+    align?: React.CSSProperties["alignItems"];
+    /** Controls `justify-content` CSS property, `'flex-start'` by default */
+    justify?: React.CSSProperties["justifyContent"];
+}
+
+/** Stack */
+const Stack = (props: Props) => {
+    const { children, setProps, ...other } = props;
+
+    return <MantineStack {...other}>{children}</MantineStack>;
+};
+
+Stack.defaultProps = {};
+
+export default Stack;
