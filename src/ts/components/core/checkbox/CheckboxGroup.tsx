4b2c6cad51fa9e42fe21a5339adb57d077866a17
Snehil Vijay <snehilvj@outlook.com>
2024-03-13 19:57:41 +0530

    Add Checkbox components

---
+++ src/ts/components/core/checkbox/CheckboxGroup.tsx
@@ -0,0 +1,54 @@
+import { Checkbox, MantineSize } from "@mantine/core";
+import { BoxProps } from "props/box";
+import { DashBaseProps, PersistenceProps } from "props/dash";
+import { InputWrapperProps } from "props/input";
+import { StylesApiProps } from "props/styles";
+import React from "react";
+
+interface Props
+    extends BoxProps,
+        StylesApiProps,
+        DashBaseProps,
+        PersistenceProps,
+        InputWrapperProps {
+    /** `Checkbox` components and any other elements */
+    children: React.ReactNode;
+    /** Controlled component value */
+    value?: string[];
+    /** Props passed down to the root element (`Input.Wrapper` component) */
+    wrapperProps?: Record<string, any>;
+    /** Controls size of the `Input.Wrapper`, `'sm'` by default */
+    size?: MantineSize | (string & {});
+    /** If set, value cannot be changed */
+    readOnly?: boolean;
+}
+
+/** CheckboxGroup */
+const CheckboxGroup = (props: Props) => {
+    const {
+        children,
+        setProps,
+        persistence,
+        persisted_props,
+        persistence_type,
+        ...other
+    } = props;
+
+    const onChange = (value: string[]) => {
+        setProps({ value });
+    };
+
+    return (
+        <Checkbox.Group onChange={onChange} {...other}>
+            {children}
+        </Checkbox.Group>
+    );
+};
+
+CheckboxGroup.defaultProps = {
+    persisted_props: ["value"],
+    persistence_type: "local",
+    value: [],
+};
+
+export default CheckboxGroup;
