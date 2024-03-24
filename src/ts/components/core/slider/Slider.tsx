185ac4113690eba7a7ad855cf3edc08e523cd736
Snehil Vijay <snehilvj@outlook.com>
2024-03-25 00:31:26 +0530

    Add slider components

---
+++ src/ts/components/core/slider/Slider.tsx
@@ -0,0 +1,110 @@
+import {
+    MantineColor,
+    MantineRadius,
+    MantineSize,
+    Slider as MantineSlider,
+    TransitionOverride,
+} from "@mantine/core";
+import { useDidUpdate } from "@mantine/hooks";
+import { BoxProps } from "props/box";
+import { DashBaseProps, PersistenceProps } from "props/dash";
+import { StylesApiProps } from "props/styles";
+import React, { useState } from "react";
+
+interface Props
+    extends BoxProps,
+        StylesApiProps,
+        DashBaseProps,
+        PersistenceProps {
+    /** Key of `theme.colors` or any valid CSS color, controls color of track and thumb, `theme.primaryColor` by default */
+    color?: MantineColor;
+    /** Key of `theme.radius` or any valid CSS value to set `border-radius`, numbers are converted to rem, `'xl'` by default */
+    radius?: MantineRadius;
+    /** Controls size of the track, `'md'` by default */
+    size?: MantineSize | (string & {}) | number;
+    /** Minimal possible value, `0` by default */
+    min?: number;
+    /** Maximum possible value, `100` by default */
+    max?: number;
+    /** Number by which value will be incremented/decremented with thumb drag and arrows, `1` by default */
+    step?: number;
+    /** Number of significant digits after the decimal point */
+    precision?: number;
+    /** Controlled component value */
+    value?: number;
+    /** Uncontrolled component default value */
+    defaultValue?: number;
+    /** Hidden input name, use with uncontrolled component */
+    name?: string;
+    /** Marks displayed on the track */
+    marks?: {
+        value: number;
+        label?: React.ReactNode;
+    }[];
+    /** Function to generate label or any react node to render instead, set to null to disable label */
+    label?: React.ReactNode | ((value: number) => React.ReactNode);
+    /** Props passed down to the `Transition` component, `{ transition: 'fade', duration: 0 }` by default */
+    labelTransitionProps?: TransitionOverride;
+    /** Determines whether the label should be visible when the slider is not being dragged or hovered, `false` by default */
+    labelAlwaysOn?: boolean;
+    /** Thumb `aria-label` */
+    thumbLabel?: string;
+    /** Determines whether the label should be displayed when the slider is hovered, `true` by default */
+    showLabelOnHover?: boolean;
+    /** Content rendered inside thumb */
+    thumbChildren?: React.ReactNode;
+    /** Disables slider */
+    disabled?: boolean;
+    /** Thumb `width` and `height`, by default value is computed based on `size` prop */
+    thumbSize?: number | string;
+    /** Determines whether track value representation should be inverted, `false` by default */
+    inverted?: boolean;
+    /** Determines when the component should update its value property. If mouseup (the default) then the slider will only trigger its value when the user has finished dragging the slider. If drag, then the slider will update its value continuously as it is being dragged. */
+    updatemode: "mouseup" | "drag";
+}
+
+/** Slider */
+const Slider = (props: Props) => {
+    const {
+        setProps,
+        updatemode,
+        value,
+        persistence,
+        persisted_props,
+        persistence_type,
+        ...other
+    } = props;
+
+    const [val, setVal] = useState(value);
+
+    useDidUpdate(() => {
+        setVal(value);
+    }, [value]);
+
+    useDidUpdate(() => {
+        if (updatemode === "drag") {
+            setProps({ value: val });
+        }
+    }, [val]);
+
+    return (
+        <MantineSlider
+            {...other}
+            value={val}
+            onChange={setVal}
+            onChangeEnd={(value) => {
+                if (updatemode === "mouseup") {
+                    setProps({ value });
+                }
+            }}
+        />
+    );
+};
+
+Slider.defaultProps = {
+    updatemode: "mouseup",
+    persisted_props: ["value"],
+    persistence_type: "local",
+};
+
+export default Slider;
