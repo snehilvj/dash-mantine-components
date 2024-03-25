65dfa09bad176969358f405d0eadacaecd99da4e
Snehil Vijay <snehilvj@outlook.com>
2024-03-25 14:23:03 +0530

    Add select components

--- src/ts/props/button.ts
+++ src/ts/props/button.ts
@@ -8,6 +8,21 @@ import { BoxProps } from "./box";
 import { LoaderProps } from "./loader";
 import { StylesApiProps } from "./styles";
 
+export interface __CloseButtonProps {
+    /** Controls width and height of the button. Numbers are converted to rem. `'md'` by default. */
+    size?: MantineSize | (string & {}) | number;
+    /** Key of `theme.radius` or any valid CSS value to set border-radius. Numbers are converted to rem. `theme.defaultRadius` by default. */
+    radius?: MantineRadius;
+    /** Sets `disabled` and `data-disabled` attributes on the button element */
+    disabled?: boolean;
+    /** `X` icon `width` and `height`, `80%` by default */
+    iconSize?: number | string;
+    /** Content rendered inside the button, for example `VisuallyHidden` with label for screen readers */
+    children?: React.ReactNode;
+    /** Replaces default close icon. If set, `iconSize` prop is ignored. */
+    icon?: React.ReactNode;
+}
+
 export interface ButtonProps extends BoxProps, StylesApiProps {
     /** Controls button `height`, `font-size` and horizontal `padding`, `'sm'` by default */
     size?: MantineSize | `compact-${MantineSize}` | (string & {});
