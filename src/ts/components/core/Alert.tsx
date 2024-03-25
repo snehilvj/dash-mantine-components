485703325ebc27be2c06d78c9b3b1ccdfb27804a
Snehil Vijay <snehilvj@outlook.com>
2024-03-25 16:52:30 +0530

    Add Alert

---
+++ src/ts/components/core/Alert.tsx
@@ -0,0 +1,65 @@
+import {
+    Alert as MantineAlert,
+    MantineColor,
+    MantineRadius,
+} from "@mantine/core";
+import { BoxProps } from "props/box";
+import { DashBaseProps } from "props/dash";
+import { StylesApiProps } from "props/styles";
+import React, { useEffect, useRef } from "react";
+
+interface Props extends BoxProps, StylesApiProps, DashBaseProps {
+    /* Content */
+    children?: React.ReactNode;
+    /** Key of `theme.radius` or any valid CSS value to set border-radius, `theme.defaultRadius` by default */
+    radius?: MantineRadius;
+    /** Key of `theme.colors` or any valid CSS color, default value is `theme.primaryColor`  */
+    color?: MantineColor;
+    /** Alert title */
+    title?: React.ReactNode;
+    /** Icon displayed next to the title */
+    icon?: React.ReactNode;
+    /** Determines whether close button should be displayed, `false` by default */
+    withCloseButton?: boolean;
+    /** Close button `aria-label` */
+    closeButtonLabel?: string;
+    /** Determines whether text color with filled variant should depend on `background-color`. If luminosity of the `color` prop is less than `theme.luminosityThreshold`, then `theme.white` will be used for text color, otherwise `theme.black`. Overrides `theme.autoContrast`. */
+    autoContrast?: boolean;
+    /** Duration in milliseconds after which the Alert dismisses itself. */
+    duration?: number;
+    /** Whether to hide the alert */
+    hide?: boolean;
+}
+
+/** Alert */
+const Alert = (props: Props) => {
+    const { children, setProps, duration, hide, ...other } = props;
+    const ref = useRef(null);
+
+    useEffect(() => {
+        if (duration) {
+            ref.current = setTimeout(() => setProps({ hide: true }), duration);
+        }
+        return () => {
+            if (ref.current) {
+                clearTimeout(ref.current);
+            }
+        };
+    }, [hide]);
+
+    const onClose = () => {
+        setProps({ hide: true });
+    };
+
+    return hide ? null : (
+        <MantineAlert {...other} onClose={onClose}>
+            {children}
+        </MantineAlert>
+    );
+};
+
+Alert.defaultProps = {
+    hide: false,
+};
+
+export default Alert;
