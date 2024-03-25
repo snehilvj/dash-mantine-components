a612c28cfedb41bfdab076003f75c41f8efe362e
Snehil Vijay <snehilvj@outlook.com>
2024-03-25 21:39:03 +0530

    Correct AreaChart

--- src/ts/components/charts/AreaChart.tsx
+++ src/ts/components/charts/AreaChart.tsx
@@ -52,51 +52,7 @@ interface Props
 const AreaChart = (props: Props) => {
     const { setProps, ...others } = props;
 
-    const data = [
-        {
-            date: "Mar 22",
-            Apples: 2890,
-            Oranges: 2338,
-            Tomatoes: 2452,
-        },
-        {
-            date: "Mar 23",
-            Apples: 2756,
-            Oranges: 2103,
-            Tomatoes: 2402,
-        },
-        {
-            date: "Mar 24",
-            Apples: 3322,
-            Oranges: 986,
-            Tomatoes: 1821,
-        },
-        {
-            date: "Mar 25",
-            Apples: 3470,
-            Oranges: 2108,
-            Tomatoes: 2809,
-        },
-        {
-            date: "Mar 26",
-            Apples: 3129,
-            Oranges: 1726,
-            Tomatoes: 2290,
-        },
-    ];
-    return (
-        <MantineAreaChart
-            h={300}
-            data={data}
-            dataKey="date"
-            series={[
-                { name: "Apples", color: "indigo.6" },
-                { name: "Oranges", color: "blue.6" },
-                { name: "Tomatoes", color: "teal.6" },
-            ]}
-            curveType="linear"
-        />
-    );
+    return <MantineAreaChart {...others} />;
 };
 
 AreaChart.defaultProps = {};
