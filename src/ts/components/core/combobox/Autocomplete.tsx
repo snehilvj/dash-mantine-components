b118b451eb9f8a8d907c524d7ff4fb51565ec67f
Snehil Vijay <snehilvj@outlook.com>
2024-03-25 14:34:02 +0530

    Add Autocomplete

---
+++ src/ts/components/core/combobox/Autocomplete.tsx
@@ -0,0 +1,66 @@
+import {
+    ComboboxStringData,
+    Autocomplete as MantineAutocomplete,
+} from "@mantine/core";
+import { useDidUpdate } from "@mantine/hooks";
+import { BoxProps } from "props/box";
+import { ComboboxLikeProps } from "props/combobox";
+import { DashBaseProps, PersistenceProps } from "props/dash";
+import { __BaseInputProps } from "props/input";
+import { StylesApiProps } from "props/styles";
+import React, { useState } from "react";
+
+interface Props
+    extends BoxProps,
+        __BaseInputProps,
+        Omit<ComboboxLikeProps, "data">,
+        StylesApiProps,
+        DashBaseProps,
+        PersistenceProps {
+    /** Data displayed in the dropdown */
+    data?: ComboboxStringData;
+    /** Controlled component value */
+    value?: string;
+}
+
+/** Autocomplete */
+const Autocomplete = (props: Props) => {
+    const { setProps, data, value, ...other } = props;
+
+    const [autocomplete, setAutocomplete] = useState(value);
+    const [options, setOptions] = useState(data);
+
+    useDidUpdate(() => {
+        setOptions(data);
+    }, [data]);
+
+    useDidUpdate(() => {
+        setProps({ data: options });
+    }, [options]);
+
+    useDidUpdate(() => {
+        setProps({ value: autocomplete });
+    }, [autocomplete]);
+
+    useDidUpdate(() => {
+        setAutocomplete(value);
+    }, [value]);
+
+    return (
+        <MantineAutocomplete
+            wrapperProps={{ autoComplete: "off" }}
+            data={options}
+            onChange={setAutocomplete}
+            value={autocomplete}
+            {...other}
+        />
+    );
+};
+
+Autocomplete.defaultProps = {
+    persisted_props: ["value"],
+    persistence_type: "local",
+    data: [],
+};
+
+export default Autocomplete;
