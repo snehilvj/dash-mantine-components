977b9d9aab7c258271b20520e1dc90b030c80eba
BSd3v <82055130+BSd3v@users.noreply.github.com>
2024-10-11 11:20:35 -0400

    updating to allow for all files to be passed from the `PRs` directory, this should allow for pages, etc.

--- PRs/app.py
+++ PRs/app.py
@@ -1,29 +1,11 @@
+from dash import *
 import dash_mantine_components as dmc
-from dash import Dash, dcc, _dash_renderer
+from dash import _dash_renderer
 
 _dash_renderer._set_react_version("18.2.0")
 
-app = Dash(external_stylesheets=dmc.styles.ALL)
+app = Dash(__name__, use_pages=True)
 
-component = dcc.Markdown(
-    """
-### PR Sample App
+app.layout = dmc.MantineProvider(page_container)
 
-This sample app uses the unreleased version of dash-mantine-components based on this pull request. You can use it to test bug fixes or try new features.
-
-### How to Use:  
-
-1.  Replace this app with your own code.  
-1.  The app is hosted live on PyCafe.  
-3. If you have a PyCafe account, you can save the app to your account. Otherwise, simply copy the URL.
-4. Share the link in the pull request or anywhere else.
-    
-    """
-)
-
-
-app.layout = dmc.MantineProvider([component])
-
-
-if __name__ == "__main__":
-    app.run(debug=True)
+app.run()
