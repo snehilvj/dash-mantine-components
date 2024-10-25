5ae3faed07f144801882d2f3745c24c289e3dc1b
Marko Marfat <marko.marfat@fer.hr>
2024-10-25 09:38:31 +0200

    added test for avatar src fix

---
+++ tests/test_avatar.py
@@ -0,0 +1,15 @@
+from dash import Dash, html, _dash_renderer
+import dash_mantine_components as dmc
+
+_dash_renderer._set_react_version("18.2.0")
+
+def test_001av_avatar(dash_duo):
+    app = Dash(__name__)
+
+    avatar = dmc.Avatar("MK")
+
+    app.layout = dmc.MantineProvider(html.Div([avatar]))
+
+    dash_duo.start_server(app)
+
+    assert dash_duo.get_logs() == []
