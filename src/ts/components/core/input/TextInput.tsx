72764a52c8df74225ec7869d3118993f69149ae1
AnnMarueW <amward@fastmail.com>
2024-10-29 12:36:46 -0700

    debounce TextInput

--- src/ts/components/core/input/TextInput.tsx
+++ src/ts/components/core/input/TextInput.tsx
@@ -1,7 +1,7 @@
 import { TextInput as MantineTextInput } from "@mantine/core";
 import { useDebouncedValue, useDidUpdate } from "@mantine/hooks";
 import { BoxProps } from "props/box";
-import { DashBaseProps, PersistenceProps } from "props/dash";
+import { DashBaseProps, PersistenceProps, DebounceProps } from "props/dash";
 import { __BaseInputProps } from "props/input";
 import { StylesApiProps } from "props/styles";
 import React, { useEffect, useState } from "react";
@@ -11,57 +11,68 @@ interface Props
         StylesApiProps,
         __BaseInputProps,
         DashBaseProps,
+        DebounceProps,
         PersistenceProps {
     /** Value for controlled input */
     value?: string;
     /** Spell check property */
     spellCheck?: boolean;
-    /** Debounce time in ms */
-    debounce?: number;
 }
 
 /** TextInput */
 const TextInput = (props: Props) => {
-    const {
-        setProps,
-        loading_state,
-        value,
-        debounce,
-        persistence,
-        persisted_props,
-        persistence_type,
-        ...others
-    } = props;
+    const { setProps, loading_state, value, n_submit, n_blur, debounce, ...others } = props;
 
     const [val, setVal] = useState(value);
-    const [debounced] = useDebouncedValue(val, debounce);
 
-    useEffect(() => {
-        setProps({ value: debounced });
+    const debounceValue = typeof debounce === 'number' ? debounce : 0;
+    const [debounced] = useDebouncedValue(val, debounceValue);
+
+    useDidUpdate(() => {
+        if (typeof debounce === 'number' || debounce === false) {
+            setProps({ value: debounced });
+        }
     }, [debounced]);
 
     useDidUpdate(() => {
         setVal(value);
     }, [value]);
 
+    const handleKeyDown = (ev) => {
+        if (ev.key === "Enter" && debounce === true) {
+            setProps({ n_submit: n_submit + 1, value: val });
+        }
+    };
+
+    const handleBlur = () => {
+        if (debounce === true) {
+            setProps({ n_blur: n_blur + 1, value: val });
+        }
+    };
+
     return (
         <MantineTextInput
             data-dash-is-loading={
                 (loading_state && loading_state.is_loading) || undefined
             }
-            {...others}
-            value={val}
-            wrapperProps={{ autoComplete: "off" }}
             onChange={(ev) => setVal(ev.currentTarget.value)}
+            value={val}
+            onKeyDown={handleKeyDown}
+            onBlur={handleBlur}
+            {...others}
         />
     );
 };
 
+
 TextInput.defaultProps = {
+    debounce: false,
     value: "",
-    debounce: 0,
     persisted_props: ["value"],
     persistence_type: "local",
+    n_submit: 0,
+    n_blur: 0,
+    autoComplete: "off"
 };
 
 export default TextInput;
