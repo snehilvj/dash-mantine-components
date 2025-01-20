74bf4f41a1ec3c21afa100dc42d72f54fae32034
Godisemo <847898+Godisemo@users.noreply.github.com>
2025-01-20 19:26:32 +0100

    Add InlineCodeHighlight component

--- PRs/app.py
+++ PRs/app.py
@@ -1,28 +1,24 @@
 import dash_mantine_components as dmc
-from dash import Dash, dcc, _dash_renderer
+from dash import Dash, _dash_renderer
 
 _dash_renderer._set_react_version("18.2.0")
 
 app = Dash(external_stylesheets=dmc.styles.ALL)
 
-component = dcc.Markdown(
-    """
-### PR Sample App
-
-This sample app uses the unreleased version of dash-mantine-components based on this pull request. You can use it to test bug fixes or try new features.
-
-### How to Use:  
-
-1.  Replace this app with your own code.  
-1.  The app is hosted live on PyCafe.  
-3. If you have a PyCafe account, you can save the app to your account. Otherwise, simply copy the URL.
-4. Share the link in the pull request or anywhere else.
-    
-    """
+app.layout = dmc.MantineProvider(
+    [
+        dmc.Text(
+            [
+                "You can highlight code inline ",
+                dmc.InlineCodeHighlight(
+                    code='<InlineCodeHighlight code="" language="tsx" />',
+                    language="tsx",
+                ),
+                " Is not that cool?",
+            ]
+        )
+    ]
 )
-
-
-app.layout = dmc.MantineProvider([component])
 
 
 if __name__ == "__main__":
