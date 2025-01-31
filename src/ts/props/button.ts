719c6607c323257b5df31cd7c65427ce30ad6f53
AnnMarueW <amward@fastmail.com>
2025-01-31 08:37:30 -0800

    update after review

--- src/ts/props/button.ts
+++ src/ts/props/button.ts
@@ -1,10 +1,8 @@
 import { MantineRadius, MantineSize } from "@mantine/core";
 import React from "react";
 
-export interface __CloseButtonProps {
-    /** Controls width and height of the button. Numbers are converted to rem. `'md'` by default. */
-    size?: MantineSize | (string & {}) | number;
-    /** Key of `theme.radius` or any valid CSS value to set border-radius. Numbers are converted to rem. `theme.defaultRadius` by default. */
+export interface __BaseButtonProps {
+     /** Key of `theme.radius` or any valid CSS value to set border-radius. Numbers are converted to rem. `theme.defaultRadius` by default. */
     radius?: MantineRadius;
     /** Sets `disabled` and `data-disabled` attributes on the button element */
     disabled?: boolean;
@@ -16,18 +14,12 @@ export interface __CloseButtonProps {
     icon?: React.ReactNode;
 }
 
+export interface __CloseButtonProps extends __BaseButtonProps {
+    /** Controls width and height of the button. Numbers are converted to rem. `'md'` by default. */
+    size?: MantineSize | (string & {}) | number;
+}
 
-export interface __ClearButtonProps {
+export interface __ClearButtonProps extends __BaseButtonProps {
     /** Size of the button, by default value is based on input context */
     size?: MantineSize | (string & {});
-    /** Key of `theme.radius` or any valid CSS value to set border-radius. Numbers are converted to rem. `theme.defaultRadius` by default. */
-    radius?: MantineRadius;
-    /** Sets `disabled` and `data-disabled` attributes on the button element */
-    disabled?: boolean;
-    /** `X` icon `width` and `height`, `80%` by default */
-    iconSize?: number | string;
-    /** Content rendered inside the button, for example `VisuallyHidden` with label for screen readers */
-    children?: React.ReactNode;
-    /** Replaces default close icon. If set, `iconSize` prop is ignored. */
-    icon?: React.ReactNode;
 }
