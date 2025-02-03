c303da7833852fc533d03684252da7fc82446afc
AnnMarueW <amward@fastmail.com>
2025-02-03 12:35:53 -0800

    update for 0.15.3 release

--- dash_mantine_components/styles.py
+++ dash_mantine_components/styles.py
@@ -1,7 +1,7 @@
-DATES = "https://unpkg.com/@mantine/dates@7.16.1/styles.css"
-CODE_HIGHLIGHT = "https://unpkg.com/@mantine/code-highlight@7.16.1/styles.css"
-CHARTS = "https://unpkg.com/@mantine/charts@7.16.1/styles.css"
-CAROUSEL = "https://unpkg.com/@mantine/carousel@7.16.1/styles.css"
-NOTIFICATIONS = "https://unpkg.com/@mantine/notifications@7.16.1/styles.css"
-NPROGRESS = "https://unpkg.com/@mantine/nprogress@7.16.1/styles.css"
+DATES = "https://unpkg.com/@mantine/dates@7.16.2/styles.css"
+CODE_HIGHLIGHT = "https://unpkg.com/@mantine/code-highlight@7.16.2/styles.css"
+CHARTS = "https://unpkg.com/@mantine/charts@7.16.2/styles.css"
+CAROUSEL = "https://unpkg.com/@mantine/carousel@7.16.2/styles.css"
+NOTIFICATIONS = "https://unpkg.com/@mantine/notifications@7.16.2/styles.css"
+NPROGRESS = "https://unpkg.com/@mantine/nprogress@7.16.2/styles.css"
 ALL = [DATES, CODE_HIGHLIGHT, CHARTS, CAROUSEL, NOTIFICATIONS, NPROGRESS]
