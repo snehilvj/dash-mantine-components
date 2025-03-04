dfad4ac7b856139c1558f8210523cafcba065fad
AnnMarueW <amward@fastmail.com>
2025-03-04 13:05:44 -0800

    remove CompositeChart add DonutChart

--- src/ts/components/charts/CompositeChart.tsx
+++ src/ts/components/charts/CompositeChart.tsx
@@ -1,3 +1,4 @@
+import { CompositeChart as MantineCompositeChart } from "@mantine/charts";
 import {
     CompositeChartSeries,
     CompositeChartCurveType,
@@ -6,12 +7,11 @@ import { BoxProps } from "props/box";
 import { GridChartBaseProps } from "props/charts";
 import { DashBaseProps } from "props/dash";
 import { StylesApiProps } from "props/styles";
-import React, { Suspense } from "react";
+import React, { useRef, useState } from "react";
+import { getClickData, isEventValid } from "../../utils/charts";
+import { getLoadingState } from "../../utils/dash3";
 
-// eslint-disable-next-line no-inline-comments
-const LazyCompositeChart = React.lazy(() => import(/* webpackChunkName: "CompositeChart" */ './fragments/CompositeChart'));
-
-export interface Props
+interface Props
     extends BoxProps,
         Omit<GridChartBaseProps, 'orientation'>,
         StylesApiProps,
@@ -65,13 +65,129 @@ export interface Props
 
 
 /** CompositeChart */
-const CompositeChart = (props: Props) => {
+const CompositeChart = ({
+    setProps,
+    loading_state,
+    clickData,
+    hoverData,
+    highlightHover = false,
+    hoverSeriesName,
+    clickSeriesName,
+    composedChartProps,
+    barProps,
+    lineProps,
+    areaProps,
+    activeDotProps,
+    ...others
+}: Props) => {
+
+    const [highlightedArea, setHighlightedArea] = useState(null);
+    const shouldHighlight = highlightHover && highlightedArea !== null;
+
+    const seriesName = useRef(null);
+
+    const onClick = (ev) => {
+        if (isEventValid(ev)) {
+            setProps({
+                clickSeriesName: seriesName.current,
+                clickData: getClickData(ev),
+            });
+        }
+        seriesName.current = null;
+    };
+
+    const onMouseOver = (ev) => {
+        if (isEventValid(ev)) {
+            setProps({
+                hoverSeriesName: seriesName.current,
+                hoverData: getClickData(ev)
+            });
+        }
+        seriesName.current = null;
+    };
+
+
+    const handleSeriesClick= (ev) => {
+        if (isEventValid(ev)) {
+            seriesName.current = ev.tooltipPayload?.[0]?.name ?? ev.name;
+        }
+    };
+
+    const handleSeriesHover = (ev) => {
+        if (isEventValid(ev)) {
+            const hoveredSeriesName = ev.tooltipPayload?.[0]?.name ?? ev.name;
+            seriesName.current = hoveredSeriesName
+            setHighlightedArea(hoveredSeriesName);
+        }
+    };
+
+    const handleDotClick = (ev, payload) => {
+        if (isEventValid(ev)) {
+            seriesName.current = payload["dataKey"];
+        }
+    }
+
+    const handleDotHover = (ev, payload) => {
+        if (isEventValid(ev)) {
+            const hoveredSeriesName = payload["dataKey"];
+            seriesName.current = hoveredSeriesName;
+            setHighlightedArea(hoveredSeriesName);
+        }
+    };
+
+    const handleHoverEnd = () => {
+        setHighlightedArea(null); // Reset highlighted area
+    };
+
+
+    const propsFunction = (item: any, chartType: "bar" | "area" | "line") => {
+        let chartProps : any = null;
+        const dimmed = shouldHighlight && highlightedArea !== item.name;
+
+        if (chartType === "bar") {
+            chartProps = barProps ?? {};
+        } else if (chartType === "area") {
+            chartProps = areaProps ?? {};
+        } else if (chartType === "line") {
+            chartProps = lineProps ?? {};
+        }
+
+        if (dimmed) {
+            chartProps.fillOpacity = 0.1;
+            chartProps.strokeOpacity = 0.2;
+        }
+
+        const returnProps: any = {
+            ...chartProps,
+            onClick: handleSeriesClick,
+            onMouseOver: handleSeriesHover,
+            onMouseOut: handleHoverEnd,
+        };
+
+        return returnProps;
+    };
+
+
+
+    const newProps = { ...composedChartProps, onClick, onMouseOver};
+
     return (
-      <Suspense fallback={null}>
-        <LazyCompositeChart {...props} />
-      </Suspense>
+        <MantineCompositeChart
+            data-dash-is-loading={getLoadingState(loading_state) || undefined}
+            composedChartProps={newProps}
+            barProps={(item) => propsFunction(item, 'bar')}   // Pass the chart type as 'bar'
+            lineProps={(item) => propsFunction(item, 'line')}  // Pass the chart type as 'line'
+            areaProps={(item) => propsFunction(item, 'area')}  // Pass the chart type as 'area'
+            activeDotProps={{
+                ...activeDotProps,
+                onClick: handleDotClick,
+                onMouseOver: handleDotHover,
+                onMouseOut: handleHoverEnd,
+            }}
+            {...others}
+        />
     );
-}
+};
 
 
 export default CompositeChart;
