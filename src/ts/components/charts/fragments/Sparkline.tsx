871737a1a59cc7ecac741a9772082e7a12086003
AnnMarueW <amward@fastmail.com>
2025-03-06 14:42:53 -0800

    add CompositeChart and Sparkline

---
+++ src/ts/components/charts/fragments/Sparkline.tsx
@@ -0,0 +1,18 @@
+import {Sparkline as MantineSparkline} from "@mantine/charts";
+import React from "react";
+import { getLoadingState } from "../../../utils/dash3";
+import { Props }  from "../Sparkline"
+
+/** Sparkline */
+const Sparkline = (props: Props) => {
+    const { setProps, loading_state, ...others } = props;
+
+    return (
+        <MantineSparkline
+            data-dash-is-loading={getLoadingState(loading_state) || undefined}
+            {...others}
+        />
+    );
+};
+
+export default Sparkline;
