6cbb658a4a233d291446feca5de4080e619c947c
Emil Haldrup Eriksen <emer@inspari.dk>
2025-03-11 21:34:44 +0100

    Re-add tiptap packages (with the correct version(!)); they are needed for type defs

--- dash_mantine_components/styles.py
+++ dash_mantine_components/styles.py
@@ -1,10 +1,10 @@
-DATES = "https://unpkg.com/@mantine/dates@7.17.1/styles.css"
-CODE_HIGHLIGHT = "https://unpkg.com/@mantine/code-highlight@7.17.1/styles.css"
-CHARTS = "https://unpkg.com/@mantine/charts@7.17.1/styles.css"
-CAROUSEL = "https://unpkg.com/@mantine/carousel@7.17.1/styles.css"
-NOTIFICATIONS = "https://unpkg.com/@mantine/notifications@7.17.1/styles.css"
-NPROGRESS = "https://unpkg.com/@mantine/nprogress@7.17.1/styles.css"
-RICH_TEXT_EDITOR = "https://unpkg.com/@mantine/tiptap@7.17.1/styles.css"
+DATES = "https://unpkg.com/@mantine/dates@7.17.0/styles.css"
+CODE_HIGHLIGHT = "https://unpkg.com/@mantine/code-highlight@7.17.0/styles.css"
+CHARTS = "https://unpkg.com/@mantine/charts@7.17.0/styles.css"
+CAROUSEL = "https://unpkg.com/@mantine/carousel@7.17.0/styles.css"
+NOTIFICATIONS = "https://unpkg.com/@mantine/notifications@7.17.0/styles.css"
+NPROGRESS = "https://unpkg.com/@mantine/nprogress@7.17.0/styles.css"
+RICH_TEXT_EDITOR = "https://unpkg.com/@mantine/tiptap@7.17.0/styles.css"
 
 ALL = [
     DATES,
