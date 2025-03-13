9cefd3f90b87669ae5c4c3cc0c49df28a527ec4c
Emil Haldrup Eriksen <emer@inspari.dk>
2025-03-13 08:52:35 +0100

    Update test (now failing, which is expected)

--- tests/test_rich_text_editor.py
+++ tests/test_rich_text_editor.py
@@ -1,6 +1,6 @@
-import time
+import json
 
-from dash import Dash, Input, _dash_renderer
+from dash import Dash, Input, Output, _dash_renderer, html
 
 import dash_mantine_components as dmc
 
@@ -8,8 +8,10 @@ _dash_renderer._set_react_version("18.2.0")
 
 
 def test_001ri_rich_text_editor(dash_duo):
-    app = Dash(__name__)
-    cid = "rich-text-editor"
+    app = Dash(__name__, prevent_initial_callbacks=True)
+    rte_id = "rich-text-editor"
+    btn_id = "click-me"
+    log_id = "output"
     content = "Hello, world!"
     to_add = " Bye, world!"
     # The expected value of the editor.
@@ -22,37 +24,49 @@ def test_001ri_rich_text_editor(dash_duo):
             }
         ],
     }
-    # Track the number of times the callback is fired.
-    counters = {"track_changes": 0}
 
     component = dmc.RichTextEditor(
         content=content,
-        extensions=["StarterKit"],
         format="json",
-        id=cid,
+        id=rte_id,
     )
-    app.layout = dmc.MantineProvider(component)
+    app.layout = dmc.MantineProvider(
+        [
+            component,
+            btn := dmc.Button("Click me", id=btn_id),
+            log := html.Div(id=log_id),
+        ]
+    )
 
     @app.callback(
-        Input(cid, "content"),
+        Output(log, "children"),
+        Input(component, "content"),
     )
     def track_changes(content):
-        counters["track_changes"] += 1
-        assert content == json_value
+        return json.dumps(content)
+
+    @app.callback(Output(component, "content"), Input(btn, "n_clicks"))
+    def set_content(n_clicks):
+        return n_clicks
 
     dash_duo.start_server(app)
 
     # Find the editor and assert its content.
-    tiptap = dash_duo.find_element(".tiptap")
-    assert tiptap.text == content
+    dash_duo.wait_for_text_to_equal(".tiptap", content)
 
     # Add text to the editor.
-    tiptap.send_keys(to_add)
-    assert tiptap.text == content + to_add
+    dash_duo.find_element(".tiptap").send_keys(to_add)
+    dash_duo.wait_for_text_to_equal(".tiptap", content + to_add)
+
+    # Check that the "track_changes" callback was fired.
+    dash_duo.wait_for_text_to_equal(f"#{log_id}", json.dumps(json_value))
+
+    # Now, check that we can write from Dash to the component
+    dash_duo.find_element(f"#{btn_id}").click()
 
     # Check that callback was fired.
-    time.sleep(0.2)
-    assert counters["track_changes"] == 1
+    dash_duo.wait_for_text_to_equal(".tiptap", "1")
+    dash_duo.wait_for_text_to_equal(f"#{log_id}", "1")
 
     # Check that no (error) logs were produced.
     assert dash_duo.get_logs() == []
