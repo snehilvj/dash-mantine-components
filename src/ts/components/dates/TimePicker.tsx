c2148af7046429e2c85c9e9b28d2e397b69456c6
AnnMarueW <amward@fastmail.com>
2025-05-12 14:53:23 -0700

    add debounce to TimePicker

--- src/ts/components/dates/TimePicker.tsx
+++ src/ts/components/dates/TimePicker.tsx
@@ -11,6 +11,7 @@ import { setPersistence, getLoadingState } from "../../utils/dash3";
 interface Props
     extends DashBaseProps,
         PersistenceProps,
+        DebounceProps,
         TimePickerProps {
     /** Value for controlled component */
     value?: string;
@@ -20,7 +21,10 @@ interface Props
 const TimePicker = ({
     setProps,
     loading_state,
+    n_submit = 0,
+    n_blur = 0,
     value = '',
+    debounce = false,
     persistence,
     persisted_props,
     persistence_type,
@@ -29,17 +33,40 @@ const TimePicker = ({
 
     const [time, setTime] = useState(value);
 
+    const debounceValue = typeof debounce === 'number' ? debounce : 0;
+    const [debounced] = useDebouncedValue(time, debounceValue);
+
     useDidUpdate(() => {
-        setProps({ value: time });
-    }, [time]);
+        if (typeof debounce === 'number' || debounce === false) {
+            setProps({ value: debounced });
+        }
+    }, [debounced]);
 
     useDidUpdate(() => {
         setTime(value);
     }, [value]);
 
+    const handleKeyDown = (ev) => {
+        if (ev.key === "Enter") {
+            setProps({
+                n_submit: n_submit + 1,
+                ...(debounce === true && { value: time }),
+            });
+        }
+    };
+
+    const handleBlur = () => {
+        setProps({
+            n_blur: n_blur + 1,
+            ...(debounce === true && { value: time })
+        });
+    };
+
     return (
         <MantineTimePicker
             data-dash-is-loading={getLoadingState(loading_state) || undefined}
+            onKeyDown={handleKeyDown}
+            onBlur={handleBlur}
             onChange={setTime}
             value={time}
             {...others}
