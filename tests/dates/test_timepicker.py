5172e95ba8f0624364f4ef16a3c96b98a09d6ae8
AnnMarueW <amward@fastmail.com>
2025-05-16 13:38:10 -0700

    add TimePicker test

--- tests/dates/test_timepicker.py
+++ tests/dates/test_timepicker.py
@@ -1,9 +1,8 @@
 import pytest
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.keys import Keys
-from dash import Dash, html, callback, Output, Input, _dash_renderer
+from dash import Dash, callback, Output, Input, _dash_renderer
 import dash_mantine_components as dmc
-import time
 
 _dash_renderer._set_react_version("18.2.0")
 
@@ -52,30 +51,30 @@ def test_001ti_timeinput(dash_duo):
     # enter a value
     elem = dash_duo.find_element("#debounce-true input")
     #time.sleep(60)
-    elem.send_keys("01", Keys.ARROW_RIGHT, "01")
+    elem.send_keys("1101")
     # verify the output has not been updated because debounce=True
     dash_duo.wait_for_text_to_equal("#out-true", "")
     elem.send_keys(Keys.TAB)
     # verify the output has been updated after input loses focus
-    dash_duo.wait_for_text_to_equal("#out-true", "01:01:00")
+    dash_duo.wait_for_text_to_equal("#out-true", "11:01:00")
 
     # debounce=False
     # verify output is blank
     dash_duo.wait_for_text_to_equal("#out-false", "")
     # enter a value
     elem = dash_duo.find_element("#debounce-false input")
-    elem.send_keys("01", Keys.ARROW_RIGHT, "01")
+    elem.send_keys("1101")
     # verify the output has been updated without pressing enter or losing focus
-    dash_duo.wait_for_text_to_equal("#out-false", "01:01:00")
+    dash_duo.wait_for_text_to_equal("#out-false", "11:01:00")
 
     # debounce is an number
     # expect that a long debounce does not call back in a short amount of time
     elem = dash_duo.find_element("#debounce-2000 input")
-    elem.send_keys("01", Keys.ARROW_RIGHT, "01")
+    elem.send_keys("1101")
     with pytest.raises(TimeoutException):
-        dash_duo.wait_for_text_to_equal("#out-2000", "01:01", timeout=1)
+        dash_duo.wait_for_text_to_equal("#out-2000", "11:01:00", timeout=1)
 
     # but do expect that it is eventually called
-    dash_duo.wait_for_text_to_equal("#out-2000", "01:01:00")
+    dash_duo.wait_for_text_to_equal("#out-2000", "11:01:00")
 
     assert dash_duo.get_logs() == []
