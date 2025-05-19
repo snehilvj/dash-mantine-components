461fa1930e3f4de510cac25d350c632c044aea2b
BSd3v <82055130+BSd3v@users.noreply.github.com>
2025-05-19 09:32:12 -0400

    creating a function for function props to iterate, instead of needing to parse each individual prop

---
+++ assets/dashMantineFunctions.js
@@ -0,0 +1,26 @@
+var dmcfuncs = window.dashMantineFunctions = window.dashMantineFunctions || {};
+
+
+dmcfuncs.myLabel = (value) => `${value} °C`
+
+
+dmcfuncs.myLabel1 = function(value) {
+    return `${value} °C`
+}
+
+dmcfuncs.myLabelComponent = function (value) {
+    const color = value <= 0 ? "var(--mantine-color-blue-3)" : "red";
+    return React.createElement(
+        'div',
+        { style: { color: color, fontSize: '16px' } },
+        value
+    );
+}
+
+dmcfuncs.labelFormatter = function (value, opts = {}) {
+  const prefix = opts.prefix || '';
+  const suffix = opts.suffix || '';
+  return `${prefix}${value}${suffix}`;
+};
+
+dmcfuncs.getScale = (v) => 2 ** v;
