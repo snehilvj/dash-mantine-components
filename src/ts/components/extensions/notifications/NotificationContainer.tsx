1bf07761799bfa55aafc966acc47ac43237c70b9
BSd3v <82055130+BSd3v@users.noreply.github.com>
2025-05-21 12:33:56 -0400

    fixing doc strings for `Notification` typing of `sendNotifications`

--- src/ts/components/extensions/notifications/NotificationContainer.tsx
+++ src/ts/components/extensions/notifications/NotificationContainer.tsx
@@ -32,20 +32,35 @@ export const appNotifications = new Proxy(appNotificationHolder, {
 });
 
 // Define the Notification interface based on your requirements
-interface Notification {
-  color?: MantineColor;
-  radius?: MantineRadius;
-  icon?: any;
-  title?: any;
-  loading?: boolean;
-  withBorder?: boolean;
-  withCloseButton?: boolean;
-  closeButtonProps?: Record<string, any>;
-  id?: string;
-  action?: 'show' | 'update' | 'hide'
-  message: any;
-  autoClose?: boolean | number;
-  position?: 'top-left' | 'top-right' | 'bottom-left' | 'bottom-right' | 'top-center' | 'bottom-center';
+interface Notification extends BoxProps, StylesApiProps {
+    /** Controls notification line or icon color, key of `theme.colors` or any valid CSS color, `theme.primaryColor` by default */
+    color?: MantineColor;
+    /** Key of `theme.radius` or any valid CSS value to set `border-radius`, `theme.defaultRadius` by default */
+    radius?: MantineRadius;
+    /** Notification icon, replaces color line */
+    icon?: React.ReactNode;
+    /** Notification title, displayed before body */
+    title?: React.ReactNode;
+    /** Replaces colored line or icon with Loader component */
+    loading?: boolean;
+    /** Determines whether notification should have a border, `false` by default */
+    withBorder?: boolean;
+    /** Determines whether close button should be visible, `true` by default */
+    withCloseButton?: boolean;
+    /** Props passed down to the close button */
+    closeButtonProps?: Record<string, any>;
+    /** Notification id, can be used to close or update notification */
+    id?: string;
+    /** Notification message, required for all notifications */
+    message: React.ReactNode;
+    /** Determines whether notification should be closed automatically,
+     *  number is auto close timeout in ms, overrides `autoClose` from `Notifications`
+     * */
+    autoClose?: boolean | number;
+    /** action */
+    action: "show" | "update" | "hide" | "clean" | "cleanQueue";
+    /** Position on the screen to display the notification.  */
+    position?: 'top-left' |  'top-right' | 'bottom-left' | 'bottom-right' | 'top-center' | 'bottom-center';
 }
 
 interface Props extends BoxProps, StylesApiProps, DashBaseProps {
