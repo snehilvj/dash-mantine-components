0607f4e5d1ed03713e442fea1da084c0a5083d2e
AnnMarueW <amward@fastmail.com>
2025-06-01 11:02:13 -0700

    fixed test (to prevent timezone shift in dates)

--- tests/assets/dashMantineFunctions.js
+++ tests/assets/dashMantineFunctions.js
@@ -27,6 +27,8 @@ dmcfuncs.labelFormatter = function (value, opts = {}) {
 dmcfuncs.getScale = (v) => 2 ** v;
 
 
-dmcfuncs.excludeDate = function(date) {
-    return date.getDay() !== 5
+dmcfuncs.excludeDate = function(dateStr) {
+    const [year, month, day] = dateStr.split("-").map(Number);
+    const parsedDate = new Date(Date.UTC(year, month - 1, day)); // Month is 0-based
+    return parsedDate.getUTCDay() !== 5;  // Use getUTCDay to avoid local timezone shift
 }
