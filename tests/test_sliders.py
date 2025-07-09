a9f934b97929633a40be406f473cf1929f131947
AnnMarueW <amward@fastmail.com>
2025-07-09 16:46:09 -0700

    fixed test

--- tests/test_sliders.py
+++ tests/test_sliders.py
@@ -6,7 +6,8 @@ from selenium.webdriver.common.by import By
 
 _dash_renderer._set_react_version("18.2.0")
 
-
+# verifies that value is correct after max is changed in a callback
+# see PR 616
 def test_001sl_slider(dash_duo):
     app = Dash(__name__)
 
@@ -43,8 +44,6 @@ def test_001sl_slider(dash_duo):
     def update(v):
         return v
 
-
-
     app.layout = dmc.MantineProvider(html.Div([component]))
 
     dash_duo.start_server(app)
@@ -56,12 +55,18 @@ def test_001sl_slider(dash_duo):
     dash_duo.find_element("#add_100").click()
     time.sleep(1)
 
-    # drag the slider to a new value
+    # Find the slider handle
     slider = dash_duo.find_element("#slider")
     handle = slider.find_element(By.CLASS_NAME, "mantine-Slider-thumb")
-    action = ActionChains(dash_duo.driver)
-    action.click_and_hold(handle).move_by_offset(75, 0).release().perform()
 
-    dash_duo.wait_for_text_to_equal("#slider-value", "108")
+    # drag the slider to a new value
+    action = ActionChains(dash_duo.driver)
+    action.click_and_hold(handle).move_by_offset(25, 0).release().perform()
+
+    # move_by_offset is based on pixels. It can result in different values based on screen size
+    # This assert verifies the value increases correctly after a max update.
+    # Prior to the fix in PR #616, dragging would incorrectly reset the value to 50.
+    value = int(dash_duo.find_element("#slider-value").text)
+    assert value > 100
 
     assert dash_duo.get_logs() == []
