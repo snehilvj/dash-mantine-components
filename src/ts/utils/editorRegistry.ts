593c75a209bbf3ce0cbe9cc423b2e24652a0f474
AnnMarueW <amward@fastmail.com>
2025-11-03 12:56:35 -0800

    fix getEditor

--- src/ts/utils/editorRegistry.ts
+++ src/ts/utils/editorRegistry.ts
@@ -1,14 +1,14 @@
 
 export const editorInstances: Record<string, any> = {};
 
-export const getEditor = (id: string): any => (id) => {
+export const getEditor = (id: string) => {
     // optional chaining so before the fragment exists it'll just return undefined
     // which does the right thing because clearly no rte is initialized yet!
-    const api = editorInstances?.id;
+    const api = editorInstances?.[id];
     if (api) {
         return api;
     }
     throw new Error(
-        `no RichTextEditor found with id: ${id}, or it's not initialized yet.`
+         `no RichTextEditor found with id: ${id}, or it's not initialized yet.`
     );
 };
