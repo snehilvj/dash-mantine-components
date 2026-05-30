9c3fdca369e16ce146e7fb8b0b4954227afb1e9e
AnnMarueW <amward@fastmail.com>
2026-05-30 08:06:06 -0700

    fix docstrings

--- src/ts/components/charts/Heatmap.tsx
+++ src/ts/components/charts/Heatmap.tsx
@@ -21,70 +21,61 @@ export interface Props extends BoxProps, StylesApiProps, DashBaseProps {
   /** Heatmap end date. Current date by default. Date is normalized to UTC midnight of the intended calendar day. */
   endDate?: Date | string;
 
-  /** If set, month labels are displayed @default false */
+  /** If set, month labels are displayed. Default false */
   withMonthLabels?: boolean;
 
   /** Month labels, array of 12 elements, can be used for localization */
   monthLabels?: string[];
 
-  /** If set, weekday labels are displayed @default false */
+  /** If set, weekday labels are displayed. Ddefault false */
   withWeekdayLabels?: boolean;
 
   /** Weekday labels, array of 7 elements, can be used for localization */
   weekdayLabels?: string[];
 
-  /** If set, trailing dates that do not fall into the given `startDate` – `endDate` range are displayed to fill empty space. @default true */
+  /** If set, trailing dates that do not fall into the given `startDate` – `endDate` range are displayed to fill empty space. Default true */
   withOutsideDates?: boolean;
 
-  /** First day of week, 0 – Sunday, 1 – Monday. @default 1 – Monday */
+  /** First day of week, 0 – Sunday, 1 – Monday. Default 1 – Monday */
   firstDayOfWeek?: 0 | 1 | 2 | 3 | 4 | 5 | 6;
 
-  /** Size of day rect in px @default 10 */
+  /** Size of day rect in px. Default 10 */
   rectSize?: number;
 
-  /** Gap between rects in px @default 1 */
+  /** Gap between rects in px. Default 1 */
   gap?: number;
 
-  /** Rect radius in px @default 2 */
+  /** Rect radius in px. Default 2 */
   rectRadius?: number;
 
   /** Colors array, used to calculate color for each value, by default 4 shades of green colors are used */
   colors?: string[];
 
-  /** Width of weekday labels column @default 30 */
+  /** Width of weekday labels column. Default 30 */
   weekdaysLabelsWidth?: number;
 
-  /** Height of month labels row @default 30 */
+  /** Height of month labels row. Default 30 */
   monthsLabelsHeight?: number;
 
-  /** Font size of month and weekday labels @default 12 */
+  /** Font size of month and weekday labels. Default 12 */
   fontSize?: number;
 
   /** A function to generate tooltip label based on the hovered rect date and value, required for the tooltip to be visible */
   getTooltipLabel?: any;
 
-  /** If set, tooltip is displayed on rect hover @default false */
+  /** If set, tooltip is displayed on rect hover. Default false */
   withTooltip?: boolean;
 
   /** Props passed down to the `Tooltip.Floating` component */
   tooltipProps?: any;
 
-  /** Props passed down to each rect depending on its date and associated value */
-  getRectProps?: any;
-
-  /** If set, inserts a spacer column between months @default false */
+  /** If set, inserts a spacer column between months. Default false */
   splitMonths?: boolean;
-
-  /** If set, legend with color levels is displayed below the heatmap @default false */
-  withLegend?: boolean;
-
-  /** Legend labels, array of 2 elements: [min label, max label] @default ['Less', 'More'] */
-  legendLabels?: [string, string];
 
   /** Clicked heatmap cell data containing date and value  */
   clickData?: Record<string, number>;
 
-  /** Hovered heatmap cell data */
+  /** Hovered heatmap cell data containing date and value */
   hoverData?: Record<string, number>;
 }
 
