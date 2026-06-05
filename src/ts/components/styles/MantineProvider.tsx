fa221f06e8f960a23ea382228fe62a9ad5a94da7
AnnMarueW <amward@fastmail.com>
2026-06-05 09:57:52 -0700

    add getStyleNonce

--- src/ts/components/styles/MantineProvider.tsx
+++ src/ts/components/styles/MantineProvider.tsx
@@ -5,6 +5,7 @@ import {
 import React from 'react';
 import * as MantineCore from '@mantine/core';
 import * as MantineHooks from '@mantine/hooks';
+import { parseFuncProps } from '../../utils/prop-functions';
 
 (window as any).MantineCore = MantineCore;
 (window as any).MantineHooks = MantineHooks;
@@ -21,6 +22,8 @@ import '@mantine/notifications/styles.css';
 interface Props extends MantineProviderProps {
     /** Unique ID to identify this component in Dash callbacks. */
     id?: string;
+    /**getStyleNonce is a function to generate nonce attribute added to dynamic generated `<style />` tags.*/
+    getStyleNonce: any;
 }
 
 /* MantineProvider */
@@ -28,7 +31,7 @@ const MantineProvider = (props: Props) => {
     const { children, ...others } = props;
 
     return (
-        <MantineMantineProvider {...others}>{children}</MantineMantineProvider>
+        <MantineMantineProvider {...parseFuncProps('MantineProvider', others)}>{children}</MantineMantineProvider>
     );
 };
 
